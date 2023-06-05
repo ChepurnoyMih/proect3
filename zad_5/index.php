9c4722b9c3ff984b55bbc2d5abea971e546436e2
ChepurnoyMih <112869962+ChepurnoyMih@users.noreply.github.com>
2023-06-05 10:16:17 +0300

    Update index.php

--- zad_5/index.php
+++ zad_5/index.php
@@ -281,9 +281,9 @@ else {
   // Проверяем меняются ли ранее сохраненные данные или отправляются новые.
     if (!empty($_COOKIE[session_name()]) && session_start() && !empty($_SESSION['login'])) {
     try {
-    $user = 'u52876'; 
-    $pass = '9106944';
-    $db = new PDO ("mysql:host=localhost;dbname=u52876", $user, $pass, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    $user = 'u52886'; 
+    $pass = '2557509';
+    $db = new PDO ("mysql:host=localhost;dbname=u52886", $user, $pass, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
     $login=$_SESSION['login'];
     $id1 = $db->query("SELECT id from log where logi='".$login."'")->fetch();
     $id1=$id1[0];
@@ -338,9 +338,9 @@ else {
     setcookie('pass', $password);
 
     try {
-    $user = 'u52876'; 
-    $pass = '9106944';
-    $db = new PDO ("mysql:host=localhost;dbname=u52876", $user, $pass, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    $user = 'u52886'; 
+    $pass = '2557509';
+    $db = new PDO ("mysql:host=localhost;dbname=u52886", $user, $pass, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
     $stmt = $db->prepare("INSERT INTO info VALUES(NULL, :fname, :email ,:bdate,:gender,:limb,:biography)");
 
     $stmt->bindParam(':fname',$_POST['name']);
