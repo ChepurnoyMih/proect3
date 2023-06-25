2ff44fd2fd50723a1d75de421cd4cd1e7b3ab61f
ChepurnoyMih <112869962+ChepurnoyMih@users.noreply.github.com>
2023-06-25 12:33:34 +0300

    Delete zad_6 directory

--- zad_6/bd.php
+++
@@ -1,7 +0,0 @@
-<?php
-    function db() {
-        $user='u52886';
-        $pass='2557509';
-        return new PDO ("mysql:host=localhost;dbname=u52886", $user, $pass, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-      }
-?>
